a6c62e9781d8c43353222a17050151197e4dc711
tam-dcreator <tam@THE-7TH>
2024-11-04 08:34:59 +0100

    Specified the version of setuptools required to build and install the package

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',  # Specify the content type for long description
     install_requires=['numpy'],
+    setup_requires=['setuptools>=65.5.1'], # Package installs properly on this version
     url='https://github.com/tam-dcreator/mypackage>',
     author='tam-dcreator',
     author_email='thebeginning724@gmail.com'
